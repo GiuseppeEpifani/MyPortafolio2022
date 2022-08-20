3262900488b86de10e9a74227ac9d30cc121874e
Guiseppe Epifani <giuseppe.epifanivillalobos@gmail.com>
2022-08-20 12:55:21 -0400

    Se agrego la logica para enviar el correo electronico

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -3,6 +3,8 @@ jQuery(document).ready(function($) {
 
   //Contact
   $('form.contactForm').submit(function() {
+    $(':input[type="submit"]').prop('disabled', true);
+
     var f = $(this).find('.form-group'),
       ferror = false,
       emailExp = /^[^\s()<>@,;:\/]+@\w[\w\.-]+\.[a-z]{2,}$/i;
@@ -88,17 +90,42 @@ jQuery(document).ready(function($) {
         i.next('.validation').html((ierror ? (i.attr('data-msg') != undefined ? i.attr('data-msg') : 'wrong Input') : '')).show('blind');
       }
     });
-    if (ferror) return false;
-    else var str = $(this).serialize();
+  
+    if (ferror) {
+      $(':input[type="submit"]').prop('disabled', false);
+      return false;
+    }
+    
+   /*  else var str = $(this).serialize();
     var action = $(this).attr('action');
     if( ! action ) {
       action = 'contactform/contactform.php';
-    }
-    $("#sendmessage").addClass("show");
+    } */
+/*     $("#sendmessage").addClass("show");
     $("#errormessage").removeClass("show");
     $('.contactForm').find("input, textarea").val("");
     $('#sendmessage').show(500);
-    $('#sendmessage').hide(5000);
+    $('#sendmessage').hide(5000); */
+
+    const serviceID = 'service_owsu7zj';
+    const templateID = 'template_rqmn4zo';
+    $('#text-send-email').html('Enviando mensaje...');
+
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        $('#text-send-email').html('Enviar mensaje');
+        $('.contactForm').find("input, textarea").val("");
+        $("#sendmessage").addClass("show");
+        $("#errormessage").removeClass("show");
+        $(':input[type="submit"]').prop('disabled', false);
+      }, (err) => {
+        $('#text-send-email').html('Enviar mensaje');
+        $("#sendmessage").removeClass("show");
+        $("#errormessage").addClass("show");
+        $('#errormessage').html(msg);
+        $(':input[type="submit"]').prop('disabled', false);
+        alert(JSON.stringify(err));
+    });
 
    /*  $.ajax({
       type: "POST",
